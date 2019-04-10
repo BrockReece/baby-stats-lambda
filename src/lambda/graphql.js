c1ec3b03f5a1e5affbdb872c2610213344bbcc19
Brock <brock.reece@croud.co.uk>
2019-04-10 18:07:52 +0100

    Add babystat payload and resolve from static json.

--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -3,6 +3,17 @@ const { ApolloServer, gql } = require("apollo-server-lambda");
 const typeDefs = gql`
   type Query {
     hello: String
+    baby(id: ID!): Baby
+  }
+
+  type Baby {
+    babystats: [ DailyStat ]
+  }
+
+  type DailyStat {
+    date: String
+    pOz: Float
+    bOz: Float  
   }
 `;
 
@@ -10,6 +21,10 @@ const resolvers = {
   Query: {
     hello: (root, args, context) => {
       return "Hello, world!";
+    },
+    baby: (root, { id }) => {
+      const { Payload } = require('../assets/payload')
+      return Payload
     }
   }
 };
