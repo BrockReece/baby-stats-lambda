bbdd26ec8d0eace5ce71eb62da7f1d24ecdff848
Brock <brock.reece@croud.co.uk>
2019-04-16 19:51:09 +0100

    Refactor logic and add title and total

--- src/components/SupplyDemandChart.vue
+++ src/components/SupplyDemandChart.vue
@@ -1,5 +1,12 @@
 <template>
-  <chart :options="chartOptionsBar"></chart>
+  <div>
+    <h2>
+      Current Milk Stock: 
+      {{ reduced[reduced.length - 1].total.toFixed(2) }}
+      <small>Oz</small>
+    </h2>
+    <chart :options="chartOptionsBar" :width="1200"></chart>
+  </div>
 </template>
 
 <script>
@@ -43,9 +50,12 @@ export default {
   },
 
   computed: {
-    chartOptionsBar() {
-      const difference = this.baby.babystats.map(s => s.pOz - s.bOz)
-      const reduced = difference.reduce((agg, cur) => {
+    difference() {
+      return this.baby.babystats.map(s => s.pOz - s.bOz)
+    },
+
+    reduced() {
+      return this.difference.reduce((agg, cur) => {
         const offset = cur < 0 ? cur : 0
         const last = agg[agg.length - 1]
         last.offset = last.total + offset
@@ -56,7 +66,9 @@ export default {
       }, [{
         total: 0,
       }])
+    },
 
+    chartOptionsBar() {
       return {
         xAxis: {
           data: this.baby.babystats.map(s => s.date)
@@ -78,7 +90,7 @@ export default {
                     color: 'rgba(0,0,0,0)'
                 }
             },
-            data: reduced.map(r => r.offset)
+            data: this.reduced.map(r => r.offset)
           },
           {
             type: 'bar',
@@ -92,7 +104,7 @@ export default {
             itemStyle: {
               color: 'green'
             },
-            data: difference.map((d) => d > 0 ? d.toFixed(2) : '-')
+            data: this.difference.map((d) => d > 0 ? d.toFixed(2) : '-')
           },
           {
             type: 'bar',
@@ -106,7 +118,7 @@ export default {
             itemStyle: {
               color: 'red'
             },
-            data: difference.map((d) => d < 0 ? (d * -1).toFixed(2) : '-')
+            data: this.difference.map((d) => d < 0 ? (d * -1).toFixed(2) : '-')
           }
         ]
       }
